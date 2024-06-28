561234afca63dd0ef19a65aa6c90341467a32658
Aparna Gupta <aparnagupta813@gmail.com>
2024-06-28 06:45:45 +0530

    updates

--- meiplay.js
+++ meiplay.js
@@ -1,35 +1,49 @@
-// import Swiper from 'swiper';
-// import 'swiper/css'
+// // import Swiper from 'swiper';
+// // import 'swiper/css'
 
 
-let videoContainer = document.getElementById("img3-container")
+// let videoContainer = document.getElementById("video-container")
 
-document.getElementById("watch").addEventListener("click", () => {
-    console.log("apple")
-    videoContainer.innerHTML = 
-    '<iframe width="1200" height="700" src="https://www.youtube.com/embed/c-I5S_zTwAc?si=BRmPf9y7WUN0zILV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>   <i class="fa-solid fa-house"></i>'
+// document.getElementById("watch").addEventListener("click", () => {
+//     console.log("apple")
+//     videoContainer.innerHTML = 
+//     ''
+//     document.getElementById("flexcont").style.display = "flex"
+//     document.getElementById("close-btn").style.fontSize = "3rem"
+//     document.getElementById("close-btn").style.color = "black"
+//     document.getElementById("close-btn").style.marginBottom = "750px"
+//     document.getElementById("close-btn").style.marginLeft = "32px"
+
+//     document.getElementById("close-btn").addEventListener("click", ()  => {
+//         document.getElementById("flexcont").style.display = "none"
+        
+
+//     })
+
+//     <div id="video-container">  <iframe width="1200" height="550" src="https://www.youtube.com/embed/c-I5S_zTwAc?si=BRmPf9y7WUN0zILV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> <p id= "close-btn"> x </p>
 
 
-}) 
+
+// }) 
 
 
-let menuScreen = document.getElementById("menubar-options")
-
-let crossButton = document.getElementById("cross-mark")
+// let menuScreen = document.getElementById("menubar-options")
 
 
 
 
-document.getElementById("menu-button").addEventListener("click", () => {
-    console.log("berries")
+
+
+// document.getElementById("menu-button").addEventListener("click", () => {
+//     console.log("berries")
     
-    menuScreen.style.display = "block"
+//     menuScreen.style.display = "block"
 
 
 
-})
+// })
 
-crossButton.addEventListener("click", () =>  {
-    menuScreen.style.display = "none"
+// crossButton.addEventListener("click", () =>  {
+//     menuScreen.style.display = "none"
 
-})
+// })
---
+++ pagedata.js
@@ -0,0 +1,10 @@
+export const dataa =  [
+    {src: "https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"}, 
+    {src: "https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"}, 
+    {src: "https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"}, 
+    {src: "https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"}, 
+    {src: "https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"}, 
+    {src: "https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg"}, 
+ 
+
+]
